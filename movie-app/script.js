756ba3b85a39f491289808de5cc840a927f4a67e
Marja Jämbeck <marja.jambeck@gmail.com>
2025-10-21 00:15:27 +0300

    updates

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,3 +1,4 @@
+//API key removed for privacy
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=YOUR_API_KEYc&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEYc&query="'
